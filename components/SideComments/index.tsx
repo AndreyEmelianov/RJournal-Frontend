eb558859be29f8182f3f0f9922d8f4b01fb15d16
Andrey Emelianov <621077@mail.ru>
2023-09-21 21:03:47 +0300

    реализовал скрытие блока комментариев в компоненте SideComments

--- components/SideComments/index.tsx
+++ components/SideComments/index.tsx
@@ -1,46 +1,25 @@
 import React from 'react';
+import clsx from 'clsx';
 
+import { CommentItem } from './CommentItem';
 import data from '../../data';
-import ArrowRightIcon from '@material-ui/icons/NavigateNextOutlined';
 
+import ArrowRightIcon from '@material-ui/icons/NavigateNextOutlined';
 import styles from './SideComments.module.scss';
 
-interface CommentItemProps {
-  user: {
-    fullname: string;
-  };
-  text: string;
-  post: {
-    title: string;
-  };
-}
-
-const CommentItem: React.FC<CommentItemProps> = ({ user, text, post }) => {
-  return (
-    <div className={styles.commentItem}>
-      <div className={styles.userInfo}>
-        <img src="https://leonardo.osnova.io/598fc957-a3f6-598c-b6f9-a033c3941d12/-/scale_crop/64x64/-/format/webp/" />
-        <a href="#">
-          <b>{user.fullname}</b>
-        </a>
-      </div>
-      <p className={styles.text}>{text}</p>
-      <a href="#">
-        <span className={styles.postTitle}>{post.title}</span>
-      </a>
-    </div>
-  );
-};
-
 export const SideComments = () => {
+  const [visible, setVisible] = React.useState(true);
+
+  const toggleVisible = () => {
+    setVisible(!visible);
+  };
+
   return (
-    <div className={styles.root}>
-      <h3>
+    <div className={clsx(styles.root, !visible && styles.rotated)}>
+      <h3 onClick={toggleVisible}>
         Комментарии <ArrowRightIcon />
       </h3>
-      {data.comments.popular.map((obj) => (
-        <CommentItem {...obj} key={obj.id} />
-      ))}
+      {visible && data.comments.popular.map((obj) => <CommentItem {...obj} key={obj.id} />)}
     </div>
   );
 };
