882008f6cdc2b574a1628cd22556193da698e7fd
Andrey Emelianov <621077@mail.ru>
2023-09-21 18:47:29 +0300

    добавил плейсхолдер для editorJs, добавил в компонент WriteForm кнопку и сделал линк, на кнопку 'новая запись' в хедере

--- components/WriteForm/index.tsx
+++ components/WriteForm/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 
-import { Input } from '@material-ui/core';
+import { Button, Input } from '@material-ui/core';
 
 import styles from './WriteForm.module.scss';
-import dynamic from 'next/dynamic';
 
 const Editor = dynamic(() => import('../Editor').then((m) => m.Editor), {
   ssr: false,
@@ -20,6 +20,9 @@ export const WriteForm: React.FC<WriteFormProps> = ({ title }) => {
       <div className={styles.editor}>
         <Editor />
       </div>
+      <Button variant="contained" color="primary">
+        Опубликовать
+      </Button>
     </div>
   );
 };
