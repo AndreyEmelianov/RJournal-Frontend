2466acc6ed8d5da952eb35ba4b5ff68600f6b528
Andrey Emelianov <621077@mail.ru>
2023-09-22 21:11:51 +0300

    закомментировал блок с профилем и автаркой в хедере, заменил на иконку неавторизованного юзера и повесил открытие окна авторизации на него

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -8,6 +8,7 @@ import {
   Menu as MenuIcon,
   ExpandMoreOutlined as ArrowBottom,
   NotificationsNoneOutlined as NotificationIcon,
+  AccountCircleOutlined as UserIcon,
 } from '@material-ui/icons';
 
 import styles from './Header.module.scss';
@@ -16,11 +17,11 @@ import { AuthDialog } from '../AuthDialog';
 export const Header: React.FC = () => {
   const [authVisible, setAuthVisible] = React.useState(false);
 
-  const handleClickOpen = () => {
+  const openAuthDialog = () => {
     setAuthVisible(true);
   };
 
-  const handleClose = () => {
+  const closeAuthDialog = () => {
     setAuthVisible(false);
   };
 
@@ -48,13 +49,13 @@ export const Header: React.FC = () => {
         </Link>
       </div>
       <div className="d-flex align-center">
-        <IconButton onClick={handleClickOpen}>
+        <IconButton>
           <MessageIcon />
         </IconButton>
         <IconButton>
           <NotificationIcon />
         </IconButton>
-        <Link href="/profile/1">
+        {/* <Link href="/profile/1">
           <a className="d-flex align-center">
             <Avatar
               className={styles.avatar}
@@ -63,9 +64,13 @@ export const Header: React.FC = () => {
             />
             <ArrowBottom />
           </a>
-        </Link>
+        </Link> */}
+        <div className={styles.loginButton} onClick={openAuthDialog}>
+          <UserIcon />
+          Войти
+        </div>
       </div>
-      <AuthDialog onClose={handleClose} visible={authVisible} />
+      <AuthDialog onClose={closeAuthDialog} visible={authVisible} />
     </Paper>
   );
 };
