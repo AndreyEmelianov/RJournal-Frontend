1f63ea2eca2fca255a34d3188c608980c8ac20ef
Andrey Emelianov <621077@mail.ru>
2023-09-22 19:49:17 +0300

    добавил id для user  и post в data.js и в компоненете CommentItem реализовал линки, которые видут на пост с этим айди или на профиль

--- data.js
+++ data.js
@@ -4,12 +4,14 @@ module.exports = {
       {
         id: 1,
         user: {
+          id: 1,
           fullname: 'Вася Пупкин',
           avatarUrl:
             'https://leonardo.osnova.io/104b03b4-5173-fd9f-2af9-b458dddc4a23/-/scale_crop/108x108/-/format/webp/',
         },
         text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
         post: {
+          id: 1,
           title: 'Какая у вас дома ванна?',
         },
         createdAt: new Date().toString(),
@@ -17,12 +19,14 @@ module.exports = {
       {
         id: 2,
         user: {
+          id: 1,
           fullname: 'Вася Пупкин',
           avatarUrl:
             'https://leonardo.osnova.io/104b03b4-5173-fd9f-2af9-b458dddc4a23/-/scale_crop/108x108/-/format/webp/',
         },
         text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
         post: {
+          id: 1,
           title: 'Какая у вас дома ванна?',
         },
         createdAt: new Date().toString(),
@@ -30,12 +34,14 @@ module.exports = {
       {
         id: 3,
         user: {
+          id: 1,
           fullname: 'Вася Пупкин',
           avatarUrl:
             'https://leonardo.osnova.io/104b03b4-5173-fd9f-2af9-b458dddc4a23/-/scale_crop/108x108/-/format/webp/',
         },
         text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
         post: {
+          id: 1,
           title: 'Какая у вас дома ванна?',
         },
         createdAt: new Date().toString(),
@@ -45,12 +51,14 @@ module.exports = {
       {
         id: 3,
         user: {
+          id: 1,
           fullname: 'Вася Пупкин',
           avatarUrl:
             'https://leonardo.osnova.io/104b03b4-5173-fd9f-2af9-b458dddc4a23/-/scale_crop/108x108/-/format/webp/',
         },
         text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
         post: {
+          id: 1,
           title: 'Какая у вас дома ванна?',
         },
         createdAt: new Date().toString(),
