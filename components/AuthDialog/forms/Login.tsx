92db2228d08ee6265a5bdef308c624d95f96cb70
Andrey Emelianov <621077@mail.ru>
2023-09-25 20:53:35 +0300

    создал папку utils, создал в ней с помощью yup простую схему для валидации, в компоненте Login подключил react-hook-form. задал ей yupResolver и отслеживаю поля ввода почты и пароля

--- components/AuthDialog/forms/Login.tsx
+++ components/AuthDialog/forms/Login.tsx
@@ -1,3 +1,7 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import { LoginFormSchema } from '../../../utils/schemas/loginValidation';
 import { Button, TextField } from '@material-ui/core';
 
 interface LoginFormProps {
@@ -5,21 +9,45 @@ interface LoginFormProps {
 }
 
 export const LoginForm: React.FC<LoginFormProps> = ({ onOpenRegister }) => {
+  const form = useForm({
+    mode: 'onSubmit',
+    resolver: yupResolver(LoginFormSchema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(form.formState.errors);
+
   return (
     <div>
-      <form>
-        <TextField className="mb-20" size="small" label="Почта" variant="outlined" fullWidth required />
+      <form onSubmit={form.handleSubmit(onSubmit)}>
         <TextField
+          {...form.register('email')}
+          name="email"
+          className="mb-20"
+          size="small"
+          label="Почта"
+          variant="outlined"
+          error={!!form.formState.errors.email?.message}
+          helperText={form.formState.errors.email?.message}
+          fullWidth
+          required
+        />
+        <TextField
+          {...form.register('password')}
           className="mb-20"
           size="small"
           label="Пароль"
           type="password"
+          name="password"
           variant="outlined"
+          error={!!form.formState.errors.password?.message}
+          helperText={form.formState.errors.password?.message}
           fullWidth
           required
         />
         <div className="d-flex align-center justify-between">
-          <Button color="primary" variant="contained">
+          <Button type="submit" color="primary" variant="contained">
             Войти
           </Button>
           <Button onClick={onOpenRegister} color="primary" variant="text">
---
+++ utils/schemas/loginValidation.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+export const LoginFormSchema = yup
+  .object({
+    email: yup.string().email('Неверная почта').required('Почта обязательная'),
+    password: yup.string().min(6, 'Пароль должен быть не менее 6 символов').required('Пароль обязательный'),
+  })
+  .required();
